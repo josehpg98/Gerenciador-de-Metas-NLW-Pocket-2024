546286f1203364f5cbcbd46b96c58f80f58a1e61
José Henrique Paludo Giombelli <42421113+josehpg98@users.noreply.github.com>
2024-09-16 17:03:13 -0300

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,105 @@
-# Gerenciador-de-Metas-NLW-Pocket-2024
-Projeto desenvolvido durante a NLW Pocket com foco em Javascript, de 09 a 13 de setembro de 2024.
+## O que esse programa faz?
+
+- Cria metas;
+- Exclui metas;
+- Lista metas (metas realizadas e pendentes);
+- Marcar e desmarca metas concluidas;
+- Persistẽncia de dados;
+- Sistema de mensagens.
+
+## Linguagem de programação utilizada
+
+A linguagem escolhida foi o Javascript, sendo facil de aprender e codificar, podendo ser usada tanto no frontend, como no backend.
+
+## Fases da resolução do problema
+
+Coletar os dados
+Processar os dados (manipular, alterar ...)
+Apresentar os dados
+
+## Peças da linguagem
+
+- Comentários
+- Declaração de variáveis (const, let)
+- Operadores (atribuição, concatenação, matemáticos, lógicos )
+- Tipos de dados (string, number, boolean)
+- Estrutura de dados (functions, object, array)
+- Controle de fluxo (if/else)
+- Estrutura de repetição (for, while)
+
+## Escopo e variáveis
+
+- Variáveis globais e locais
+- Constantes
+
+## Tipos de dados
+
+- Strings (texto): "" '' ``
+- Numbers: 2 1.4
+- Boolean: true, false
+
+## Operadores
+
+- Operadores de atribuição de valor: =
+- Operador de contatenação: +
+- Operadores de comparação: == != <= >= < >
+- Spread operator: ...
+
+## Estruturas de dados
+
+### Arrays 
+
+- Lista que contém qualquer tipo de dado
+- Métodos de array: push, [find, forEach, filter, map] : HOF (Higher Order Functions)
+
+### Objetos
+
+- Acesso a propriedades de objetos
+- Atributos e métodos
+- Criação e manipulação de objetos
+
+### Functions
+
+- Criar, passar argumento
+- Excutar
+- Arrow function / named function
+
+## Estrutura de repetição
+
+- While
+
+## Condicionais
+
+- Switch
+- if/else
+
+## Módulos em Node.js:
+
+- Importação de módulos (require, CommonJS)
+- Biblioteca 'inquirer' para criar prompts interativos
+- FS (file system)
+
+## JSON
+
+- Javascript Object Notation (.json)
+- JSON.parse(): transforma de JSON para JS
+- JSON.stringify(): transforma de JS para JSON
+
+## Programação assíncrona e Promises:
+
+- Uso de funções assíncronas (async/await)
+
+## Execução e instalar Dependências
+
+- npm install inquirer
+- Executar o projeto: node index.js
+
+## Certificado
+
+- Certificado em Português Brasileiro
+
+![Certificado](Certificado-NLW-Pocket-2024-Pt-Br.png)
+
+- Certificado em Inglês
+
+![Certificado](Certificado-NLW-Pocket-2024-En.png)
---
+++ index.js
@@ -0,0 +1,221 @@
+const { select, input, checkbox } = require('@inquirer/prompts')
+const fs = require("fs").promises
+
+let mensagem = "Bem vindo ao App de Metas - NLW Pocket";
+
+let metas
+
+///arrow function assincrona para carregar metas
+///onde é chamada na fubção start, e devolve após realizar as tarefas
+const carregarMetas = async () => {
+    ///lê metas de um arquivo .json
+    try {
+        const dados = await fs.readFile("metas.json", "utf-8")
+        metas = JSON.parse(dados)
+    }
+    ///tratamento em casso de erros
+    catch (erro) {
+        console.log("Erro ao carregar o arquivo. -> "+erro)
+        metas = []
+    }
+}
+
+///salva as metas no arquivo .json
+const salvarMetas = async () => {
+    await fs.writeFile("metas.json", JSON.stringify(metas, null, 2))
+}
+
+///função para adicionar metas
+const cadastrarMeta = async () => {
+    const meta = await input({ message: "Digite a meta que deseja criar:" })
+
+    if (meta.length == 0) {
+        mensagem = 'A meta não pode estar em branco!'
+        return
+    }
+
+    ///coloca a meta sempre como não concluida
+    metas.push(
+        { value: meta, checked: false }
+    )
+    mensagem = "Meta cadastrada com sucesso!"
+}
+
+///função para listar as metas
+const listarMetas = async () => {
+    if (metas.length == 0) {
+        mensagem = "Não existem metas para mostrar!"
+        return
+    }
+
+    const respostas = await checkbox({
+        message: "- Use as setas para mudar de meta | Espaço para marcar ou desmarcar | Enter para finalizar a etapa e voltar ao menu.",
+        choices: [...metas],
+        instructions: false,
+    })
+
+    ///verifica as metas não concluidas
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    if (respostas.length == 0) {
+        mensagem = "Nenhuma meta selecionada!"
+        return
+    }
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    mensagem = 'Meta(s) marcada(s) como concluída(s)!'
+
+}
+
+const metasRealizadas = async () => {
+    if (metas.length == 0) {
+        mensagem = "Não existem metas!"
+        return
+    }
+
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+    })
+
+    if (realizadas.length == 0) {
+        mensagem = 'Não existem metas realizadas! :('
+        return
+    }
+
+    await select({
+        message: "Metas Realizadas: " + realizadas.length,
+        choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    if (metas.length == 0) {
+        mensagem = "Não existem metas!"
+        return
+    }
+
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
+
+    if (abertas.length == 0) {
+        mensagem = 'Não existem metas abertas! :)'
+        return
+    }
+
+    await select({
+        message: "Metas Abertas: " + abertas.length,
+        choices: [...abertas]
+    })
+}
+
+const deletarMetas = async () => {
+    if(metas.length == 0) {
+        mensagem = "Não existem metas!"
+        return
+    }
+
+    const metasDesmarcadas = metas.map((meta) => {
+        return { value: meta.value, checked: false }
+    })
+
+    const itemsADeletar = await checkbox({
+        message: "Selecione item para deletar",
+        choices: [...metasDesmarcadas],
+        instructions: false,
+    })
+
+    if (itemsADeletar.length == 0) {
+        mensagem = "Nenhum item para deletar!"
+        return
+    }
+
+    itemsADeletar.forEach((item) => {
+        metas = metas.filter((meta) => {
+            return meta.value != item
+        })
+    })
+
+    mensagem = "Meta(s) deleta(s) com sucesso!"
+}
+
+const mostrarMensagem = () => {
+    console.clear();
+
+    if (mensagem != "") {
+        console.log(mensagem)
+        console.log("")
+        mensagem = ""
+    }
+}
+
+const start = async () => {
+    await carregarMetas()
+
+    while (true) {
+        mostrarMensagem()
+        await salvarMetas()
+
+        const opcao = await select({
+            message: "Opções do Menu do Programa de Metas >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Metas realizadas",
+                    value: "realizadas"
+                },
+                {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
+                    name: "Deletar metas",
+                    value: "deletar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
+        switch (opcao) {
+            case "cadastrar":
+                await cadastrarMeta()
+                break
+            case "listar":
+                await listarMetas()
+                break
+            case "realizadas":
+                await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
+                break
+            case "deletar":
+                await deletarMetas()
+                break
+            case "sair":
+                console.log('Programa encerrado com sucesso!')
+                return
+        }
+    }
+}
+
+start();
